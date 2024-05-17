87fcdd98ee6cf5dccb1544df7b6968a2efc02c6e
edwards <156817117+kenwayedwards@users.noreply.github.com>
2024-05-17 22:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,171 @@
 import os
-import streamlit as st
-import speech_recognition as sr
-
-st.title("Speech-to-Text Transcription App")
-
-# Function for speech to text
-def speech_to_text():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Speak Anything...")
-        audio = recognizer.listen(source)
-    try:
-        text = recognizer.recognize_google(audio)
-        st.write("You said: {}".format(text))
-    except sr.UnknownValueError:
-        st.error("Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        st.error("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# Call the speech-to-text function
-speech_to_text()
+import queue
+import re
+import sys
+import time
+import pyaudio
+from google.oauth2 import service_account
+from google.cloud import speech
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']= 'woven-alpha-364513-06bd95639b48.json'
+client = speech.SpeechClient()
+STREAMING_LIMIT = 240000  # 4 minutes
+SAMPLE_RATE = 16000
+CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
+def get_current_time() -> int:
+    return int(round(time.time() * 1000))
+class ResumableMicrophoneStream:
+    def __init__(
+        self: object,
+        rate: int,
+        chunk_size: int,
+    ) -> None:
+        self._rate = rate
+        self.chunk_size = chunk_size
+        self._num_channels = 1
+        self._buff = queue.Queue()
+        self.closed = True
+        self.start_time = get_current_time()
+        self.restart_counter = 0
+        self.audio_input = []
+        self.last_audio_input = []
+        self.result_end_time = 0
+        self.is_final_end_time = 0
+        self.final_request_end_time = 0
+        self.bridging_offset = 0
+        self.last_transcript_was_final = False
+        self.new_stream = True
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=self._num_channels,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self.chunk_size,
+            stream_callback=self._fill_buffer,
+        )
+    def __enter__(self: object) -> object:
+        self.closed = False
+        return self
+    def __exit__(
+        self: object,
+        type: object,
+        value: object,
+        traceback: object,
+    ) -> object:
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        self._buff.put(None)
+        self._audio_interface.terminate()
+    def _fill_buffer(
+        self: object,
+        in_data: object,
+        *args: object,
+        **kwargs: object,
+    ) -> object:
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+    def generator(self: object) -> object:
+        while not self.closed:
+            data = []
+            if self.new_stream and self.last_audio_input:
+                chunk_time = STREAMING_LIMIT / len(self.last_audio_input)
+                if chunk_time != 0:
+                    if self.bridging_offset < 0:
+                        self.bridging_offset = 0
+                    if self.bridging_offset > self.final_request_end_time:
+                        self.bridging_offset = self.final_request_end_time
+                    chunks_from_ms = round(
+                        (self.final_request_end_time - self.bridging_offset)
+                        / chunk_time
+                    )
+                    self.bridging_offset = round(
+                        (len(self.last_audio_input) - chunks_from_ms) * chunk_time
+                    )
+                    for i in range(chunks_from_ms, len(self.last_audio_input)):
+                        data.append(self.last_audio_input[i])
+                self.new_stream = False
+            chunk = self._buff.get()
+            self.audio_input.append(chunk)
+            if chunk is None:
+                return
+            data.append(chunk)
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                    self.audio_input.append(chunk)
+                except queue.Empty:
+                    break
+            yield b"".join(data)
+def listen_print_loop(responses: object, stream: object) -> None: 
+    for response in responses:
+        if get_current_time() - stream.start_time > STREAMING_LIMIT:
+            stream.start_time = get_current_time()
+            break
+        if not response.results:
+            continue
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+        transcript = result.alternatives[0].transcript
+        result_seconds = 0
+        result_micros = 0
+        if result.result_end_time.seconds:
+            result_seconds = result.result_end_time.seconds
+        if result.result_end_time.microseconds:
+            result_micros = result.result_end_time.microseconds
+        stream.result_end_time = int((result_seconds * 1000) + (result_micros / 1000))
+        corrected_time = (
+            stream.result_end_time
+            - stream.bridging_offset
+            + (STREAMING_LIMIT * stream.restart_counter)
+        )
+        if result.is_final:
+            sys.stdout.write(transcript + "\n")
+            stream.is_final_end_time = stream.result_end_time
+            stream.last_transcript_was_final = True
+            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                sys.stdout.write("Exiting...\n")
+                stream.closed = True
+                break
+        else:
+            sys.stdout.write(transcript + "\r")
+            stream.last_transcript_was_final = False
+def main() -> None:
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=SAMPLE_RATE,
+        language_code="en-US",
+        max_alternatives=1,
+    )
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
+    sys.stdout.write('\nListening, say "Quit" or "Exit" to stop.\n\n')
+    with mic_manager as stream:
+        while not stream.closed:
+            stream.audio_input = []
+            audio_generator = stream.generator()
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
+            responses = client.streaming_recognize(streaming_config, requests)
+            listen_print_loop(responses, stream)
+            if stream.result_end_time > 0:
+                stream.final_request_end_time = stream.is_final_end_time
+            stream.result_end_time = 0
+            stream.last_audio_input = []
+            stream.last_audio_input = stream.audio_input
+            stream.audio_input = []
+            stream.restart_counter = stream.restart_counter + 1
+            if not stream.last_transcript_was_final:
+                sys.stdout.write("\n")
+            stream.new_stream = True
+if __name__ == "__main__":
+    main()
